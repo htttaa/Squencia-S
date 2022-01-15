0df5c1c7500c5a410e241e256909d2319716f38c
Hta <agth567@gmail.com>
2022-01-14 21:47:04 -0300

    Adicionei-SequenciaS

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System; 
+
+class DIO {
+
+    static void Main(string[] args) { 
+
+ double a, c, S= 1;
+            for(int i=2; i<101;i++)
+            {  
+               S += 1.0/i;
+            }
+            a = Math.Round(S, 2);
+            Console.WriteLine(a);
+    }
+
+}
